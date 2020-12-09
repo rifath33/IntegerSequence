83a66312c3cca7dcb2cbef1ce2c7fed8f039a787
Rifath <hrifath20@stuy.edu>
2020-12-09 10:25:47 -0500

    second test case

--- Tester.java
+++ Tester.java
@@ -1,7 +1,7 @@
 public class Tester{
   public static void main(String[] args){
 
-    IntegerSequence r = new Range(10,15);
+    IntegerSequence r = new Range(10,20);
     while(r.hasNext()){
       System.out.print(r.next());
       if( r.hasNext() ){
