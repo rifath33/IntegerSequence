38212622c1aca40a3a9df9d5107e97149bb0e7fb
Rifath <hrifath20@stuy.edu>
2020-12-09 10:36:51 -0500

    created the java file now lets get started shall we

---
+++ ArraySequence.java
@@ -0,0 +1,40 @@
+/*
+You are adding onto your NumberSequence lab. I will give you the majority of the period and homework for this to be completed.
+
+Range does not allow for non-consecutive values, however non-consecutive values are still sequences, so we need a new implementation of this interface:
+public class ArraySequence implements IntegerSequence
+This implementation will unfortunately have to store all of the values, but can be used interchangeably with Range.
+
+Similar to before you will throw a NoSuchElementException when the user tries to get the next value when there are no more values left.
+*/
+
+import java.util.NoSuchElementException;
+public class ArraySequence implements IntegerSequence{
+  private int currentIndex;
+  private int []data;
+
+  /*Construct the sequence by copying values from the other array into the data array*/
+  public ArraySequence(int [] other){
+    data = other;
+    currentIndex = 0;
+  }
+
+
+  public void reset(){
+  }
+
+
+  public int length(){
+    return 1;
+  }
+
+  public boolean hasNext(){
+    return false;
+  }
+
+  //@throws NoSuchElementException
+  public int next() throws NoSuchElementException{
+    return 1;
+  }
+
+}
