b9950587c0df06087942acaafa2b50be26b41e88
rifath33 <hrifath20@stuy.edu>
2021-01-05 05:05:27 -0500

    fixed issue with data=other now i know thats bad...

--- ArraySequence.java
+++ ArraySequence.java
@@ -15,7 +15,10 @@ public class ArraySequence implements IntegerSequence{
 
   /*Construct the sequence by copying values from the other array into the data array*/
   public ArraySequence(int [] other){
-    data = other;
+    data = new int[other.length];
+		for (int i = 0; i < other.length; i++) {
+			data[i] = other[i];
+		}
     currentIndex = 0;
   }
 
